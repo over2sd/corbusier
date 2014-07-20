b21b1dec7030cf2c3cb5e145551363cff6e1901f
Lincoln Sayger <skrey_pub@cox.net>
2014-07-20 00:51:19 -0500

    Modularize SVG generation, map generation; added seed # to image.

--- MapDraw.pm
+++ MapDraw.pm
@@ -29,27 +29,42 @@ sub incColor {
 
 sub formSVG {
 	print "Forming SVG...";
-    my ($w,$h,$linesr,$boxr) = @_;
+    my ($w,$h,$linesr,$boxr,%exargs) = @_;
+	my ($offsetx,$offsety,$showseed,$seed) = (0,0,0,0);
+	if (defined $exargs{'seed'}) {
+		$seed = $exargs{'seed'};
+		$showseed = 1;
+	}
+	if (defined $exargs{'xoff'}) {
+		$offsetx = $exargs{'xoff'};
+	}
+	if (defined $exargs{'yoff'}) {
+		$offsety = $exargs{'yoff'};
+	}
     my @lines = @$linesr;
-    # declare SVG document type:
-#    my $out = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20000303 Stylable//EN\" \"http://www.w3.org/TR/2000/03/WD-SVG-20000303/DTD/svg-20000303-stylable.dtd\">\n";
-    my $out = "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n";
-    $out = "$out<svg width=\"100%\" height=\"100%\" version=\"1.0\" xmlns=\"http://w3.org/2000/svg\">\n";
-	$out = "$out    <rect height=\"$h\" width=\"$w\" fill=\"#fff\" />\n";
+	my $out = "    <rect x=\"$offsetx\" y=\"$offsety\" height=\"$h\" width=\"$w\" fill=\"#fff\" />\n";
     foreach my $i (0 .. $#lines) {
         my $line = $lines[$i];
         my $curcol = incColor(); # later, do this only when switching road types, or not at all.
-        $out = "$out    <line x1=\"" . $line->ox() . "\" y1=\"" . $line->oy() . "\" x2=\"" . $line->ex() . "\" y2=\"" . $line->ey() . "\" stroke=\"$curcol\" />\n";
+        $out = sprintf("$out    <line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"$curcol\" />\n",$line->ox() + $offsetx,$line->oy() + $offsety,$line->ex() + $offsetx,$line->ey() + $offsety);
     }
-    $out = "$out</svg>\n";
+	if ($showseed) {
+		my $boxw = 160;
+		$out = sprintf("$out	<rect x=\"%d\" y=\"%d\" width=\"$boxw\" height=\"40\" fill=\"#6ff\" />\n	<text x=\"%d\" y=\"%d\" font-size=\"3.2em\" fill=\"#00c\">#$seed</text>\n",$offsetx + $w - $boxw,$offsety,$offsetx + $w - $boxw + 5,$offsety + 35);
+	}
     return $out; 
 }
 
 sub saveSVG {
     my ($data,$filename) = @_;
 	print "Writing SVG to '$filename'...";
+    # declare SVG document type:
+#    my $out = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20000303 Stylable//EN\" \"http://www.w3.org/TR/2000/03/WD-SVG-20000303/DTD/svg-20000303-stylable.dtd\">\n";
+    my $out = "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n";
+    $out = "$out<svg width=\"100%\" height=\"100%\" version=\"1.0\" xmlns=\"http://w3.org/2000/svg\">\n";
+    $out = "$out$data</svg>\n";
     open (OUTFILE, ">$filename") || return (-1,$!);
-    print OUTFILE $data;
+    print OUTFILE $out;
     return 0;
 }
 
--- main.pl
+++ main.pl
@@ -7,6 +7,23 @@ use MapDraw;
 
 use Getopt::Long;
 
+sub mapSeed {
+		my ($highways,$secondaries,$ratio,$pointsofinterest,$maxroads,$width,$height,$seed,$showtheseed) = @_;
+		srand($seed);
+        print "Using map seed $seed...\n";
+        my ($nr,@routes) = MapDes::genmap($highways,$secondaries,$ratio,$pointsofinterest,$maxroads,$width,$height);
+#        my @poi = 
+        print "- $nr routes\n";
+        foreach my $i (0 .. $#routes) {
+            print $routes[$i]->describe(1) . "\n";
+        }
+		my @boxen;
+		my %exargs;
+		if ($showtheseed) { $exargs{'seed'} = $seed; }
+        my $out = MapDraw::formSVG($width,$height,\@routes,\@boxen,%exargs);
+		return $out;
+}
+
 sub main {
     my $gui = 0; # show the gui?
     my $hiw = 5; # number of highways leading out of town
@@ -15,15 +32,18 @@ sub main {
     my $depth = 3; # how many substreets deep should we go?
     my $poi = 0; # use points of interest instead of branching
     my $max = 0; # maximum number of roads in total. If not set as an option, this will be equal to highways * secondaries.
-    my $seed = 0; # Random seed/map number
+    my $seed = -1; # Random seed/map number
     my $w = 800; # Width of image
     my $h = 600; # Height of image
     my $fn = 'output.svg'; # Filename of map
+	my $disp = 0; # display seed on map (debug function)
+							$disp = 1; # for development. TODO: Remove this line when done tweaking generator
     GetOptions(
         'gui' => \$gui,
         'depth|d=i' => \$depth,
         'highways|exits|e=i' => \$hiw,
         'file|f=s' => \$fn,
+		'listseed|l' => \$disp,
         'map|m=i' => \$seed,
         'poimode|p' => \$poi,
         'ratio|r=i' => \$rat,
@@ -32,21 +52,27 @@ sub main {
         'w=i' => \$w,
         'h=i' => \$h
         );
-    if ($seed == 0) { $seed =  time }
-    srand($seed);
+    if ($seed < 0) { $seed =  time }
     if (!$max) { $max = $hiw * $sec; }
     if ($gui) {
 #        use MapGUI;
         showGUI();
     } else {
-        print "Using map seed $seed...\n";
-        my ($nr,@routes) = MapDes::genmap($hiw,$sec,$rat,$poi,$max,$w,$h);
-#        my @poi = 
-        print "- $nr routes\n";
-        foreach my $i (0 .. $#routes) {
-            print $routes[$i]->describe(1) . "\n";
-        }
-        my $svg = MapDraw::formSVG($w,$h,\@routes);
+		my $svg = '';
+#		 if (@seedlist) {
+# Start of seed loop
+#			my @svglist;
+#			foreach my $seed (@seedlist) {
+#				my $svgstring= mapSeed($hiw,$sec,$rat,$poi,$max,$w,$h,$seed,$disp);
+#				push(@svglist,$svgstring);
+# End of seed loop
+#			}
+#			foreach my $add (@svglist) {
+#				$svg = "$svg$add\n";
+#			}
+#		} else {
+			$svg = mapSeed($hiw,$sec,$rat,$poi,$max,$w,$h,$seed,$disp);
+#		}
         my ($result,$errstr) = MapDraw::saveSVG($svg,$fn);
         if ($result != 0) { print "Map could not be saved: $errstr"; }
         print "\n";
